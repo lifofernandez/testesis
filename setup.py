aa7785df865d52752dad4034e57127445709791e
lf <lifofernandez@gmail.com>
2019-07-23 16:22:53 -0300

    cambios menores

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+# you may need setuptools instead of distutils
+
+setup(
+    name             = 'yml2mid',
+    version          = '0.1',
+    description      = 'Compose MIDI sequences using YAML definitions.',
+    author           = 'Man Foo',
+    author_email     = 'foomail@foo.com',
+    packages         = ['yml2mid'],  #same as name
+    #packages         = find_packages(),
+    install_requires = [
+      'pyyaml',
+      'midiutil',
+    ]
+    scripts = [
+      'argumentos.py',
+      'articulacion.py',
+      'elemento.py',
+      'main.py',
+      'pista.py',
+      'seccion.py',
+      'segmento.py',
+      'yml2mid',
+    ]
+)
+
